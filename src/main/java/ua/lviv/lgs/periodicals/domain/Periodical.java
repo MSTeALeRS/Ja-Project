df640a223dd03c29440d467c0d92c362ed39d6b3
MStealers <Fresh5956111@gmail.com>
2021-09-21 22:05:22 +0300

    add login

--- src/main/java/ua/lviv/lgs/periodicals/domain/Periodical.java
+++ src/main/java/ua/lviv/lgs/periodicals/domain/Periodical.java
@@ -1,10 +1,27 @@
 package ua.lviv.lgs.periodicals.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "periodical")
 public class Periodical {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column
     private String name;
+
+    @Column
     private String description;
+
+    @Column
     private Double price;
 
     public Periodical() {
@@ -103,4 +120,3 @@ public class Periodical {
         return "Periodical [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
     }
 }
-
