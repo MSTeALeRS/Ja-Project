f180cd70123d9cc294c65587a8799e6e665dd336
MStealers <Fresh5956111@gmail.com>
2021-09-26 15:16:11 +0300

    add logger

--- src/main/java/ua/lviv/lgs/University/service/EvaluationService.java
+++ src/main/java/ua/lviv/lgs/University/service/EvaluationService.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.University.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.lviv.lgs.University.dao.EvaluationRepository;
@@ -9,19 +11,22 @@ import java.util.List;
 
 @Service
 public class EvaluationService {
+    private Logger logger = LoggerFactory.getLogger(EvaluationService.class);
 
     @Autowired
      private EvaluationRepository evaluationRepository;
 
     public Evaluation save(Evaluation ev){
+        logger.info("add new Evaluation {} : " + ev);
         return evaluationRepository.save(ev);
     }
 
     public List<Evaluation> getAllEvaluation(){
-
+        logger.info("find all Evaluation {} : " );
         return evaluationRepository.findAll();
     }
     public Evaluation findById(Integer id) {
+        logger.info("find Evaluation by {} : " + id);
         return evaluationRepository.findById(id).get();
     }
 }
--- src/main/java/ua/lviv/lgs/University/service/StudentService.java
+++ src/main/java/ua/lviv/lgs/University/service/StudentService.java
@@ -2,6 +2,8 @@ package ua.lviv.lgs.University.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +13,23 @@ import ua.lviv.lgs.University.domain.Student;
 
 @Service
 public class StudentService {
-
+    private Logger logger = LoggerFactory.getLogger(StudentService.class);
     @Autowired
     private StudentRepository studentRepository;
 
     public Student save(Student student){
-    return studentRepository.save(student);
+        logger.info("add new Student {} : " + student);
+        return studentRepository.save(student);
     }
 
     public List<Student> getAllStudents(){
-
+        logger.info("get all Students {} : " );
 
         return studentRepository.findAll();
     }
 
     public Student findById(Integer id) {
+        logger.info("find Student by {} : " + id);
         return studentRepository.findById(id).get();
     }
 
